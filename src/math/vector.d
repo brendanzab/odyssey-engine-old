9c004aeb6ff91c73e4ba298da1ea2ee29ecc2b69
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2012-07-02 22:26:34 +1000

    improvements to vector class

--- src/math/vector.d
+++ src/math/vector.d
@@ -16,8 +16,6 @@ const {
  */
 struct Vec3 {
 
-    /* Member Variables */
-
     float x, y, z;
 
     /* Operations */
@@ -42,9 +40,21 @@ struct Vec3 {
         return Vec3(x*f, y*f, z*f);
     }
 
-    /// Dot product: `this * v `
+    /// Returns the dot product: `this * v `
     float opBinary(string op : "*")(Vec3 v) {
         return (x * v.x) + (y * v.y) + (z * v.z);
+    }
+    
+    /// Returns the vector divided by a float: `this * f`
+    Vec3 opBinary(string op : "/")(float f) {
+        f = 1/f;
+        return Vec3(x*f, y*f, z*f);
+    }
+    
+    /// Returns the vector divided by another vector `this / v`
+    float opBinary(string op : "/")(Vec3 v) {
+        f = 1/f;
+        return Vec3(x * v.x, y * v.y, z * v.z);
     }
 
     Vec3 cross(Vec3 v) {
@@ -55,6 +65,10 @@ struct Vec3 {
     }
 
     /* Assignment Operations */
+    
+    void opAssign()(float f) {
+        x = y = z = f;
+    }
 
     /// Add to vector: `this += v`
     void opAssign(string op : "+")(Vec3 v) {
@@ -77,17 +91,39 @@ struct Vec3 {
         z *= f;
     }
 
-    /* Other Methods */
+    /// Divide vector: `this /= f`
+    void opAssign(string op : "/")(float f) {
+        f = 1/f;
+        x *= f;
+        y *= f;
+        z *= f;
+    }
 
-    /// Returns the magnitude of the vector
-    float magnitude() {
+    /// Divide from vector: `this /= v`
+    void opAssign(string op : "/")(Vec3 v) {
+        x /= v.x;
+        y /= v.y;
+        z /= v.z;
+    }
+
+    /* Other Methods */
+    
+    @property float magnitude() {
         return sqrt(x*x + y*y + z*z);
+    }
+    
+    /// Scale the magnitude to the specified size
+    @property void magnitude(float m) {
+        float n = (1 / magnitude) * m;
+        x *= n;
+        y *= n;
+        z *= n;
     }
 
     /// Returns the normalized vector
     Vec3 normalize() {
         float n = 1 / magnitude;
-        return Vec3(x*n, y*n, z*n);
+        return this * n;
     }
 
     void normalizeSelf() {
@@ -99,31 +135,44 @@ struct Vec3 {
     
 }
 
-string toString(Vec3 v) {
-    return format("(", v.x, ", ", v.y, ", ",  v.z, ")");
-}
-
+// Yeah, I have no idea how to write unittests. So I'm 
+// just printing things out at the moment... :P
 unittest {
+    
+    import std.stdio;
     
     Vec3 v1 = { 1, 2,  3 };
     Vec3 v2 = {-1, 2, -2 };
     float f = 3.4;
+    
+    assert(-v2     == Vec3(1, -2, 2));
+    assert(v1 + v2 == Vec3(0, 4, 1));
+    assert(v1 - v2 == Vec3(2, 0, 5));
+    //assert(v1 * f  == Vec3(3.4, 6.8, 10.2));
+    assert(v1 * v2 == -3);
 
     // Test vector operations
     writeln("Test Vec3 Operations:");
     writeln;
-    writeln("let v1 = ", v1.toString);
-    writeln("let v2 = ", v2.toString);
-    writeln("let f = ", f);
+    writeln("let v1 = ", v1);
+    writeln("let v2 = ", v2);
+    writeln("let f  = ", f);
     writeln;
-    writeln("\b-v1",        "\t\t= ",   -v1);
-    writeln("v1 + v2",      "\t\t= ",   (v1 + v2).toString);
-    writeln("v1 - v2",      "\t\t= ",   (v1 - v2).toString);
-    writeln("v1 * f",       "\t\t= ",   v1 * f);
-    writeln("v1 * v2",      "\t\t= ",   (v1 * v2).toString);
+    writeln("-v1          = ",   -v1);
+    writeln("v1 + v2      = ",   v1 + v2);
+    writeln("v1 - v2      = ",   v1 - v2);
+    writeln("v1 * f       = ",   v1 * f);
+    writeln("v1 * v2      = ",   v1 * v2);
     writeln;
-    writeln("v1.normalize", "\t= ",     v1.normalize);
-    writeln("v1.magnitude", "\t= ",     v1.magnitude);
-    writeln("v1.cross(v2)", "\t= ",     v1.cross(v2));
+    writeln("v1.normalize = ",     v1.normalize);
+    writeln("v1.magnitude = ",     v1.magnitude);
+    writeln("v1.cross(v2) = ",     v1.cross(v2));
+    writeln;
     
+    v1.magnitude = 5;
+    writeln("v1.magnitude = 5");
+    writeln("v1 = ", v1);
+    v1 = 0;
+    writeln("v1 = 0");
+    writeln("v1 = ", v1);
 }
