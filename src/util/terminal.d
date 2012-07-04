8ab4982b94ed04af42d664df1dfa1a03d68745bc
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2012-07-05 02:36:45 +1000

    fixed prettyprinting

--- src/util/terminal.d
+++ src/util/terminal.d
@@ -9,32 +9,32 @@ module odyssey.util.terminal;
 version(Posix) enum TERM_OS_Posix = true;
 else enum TERM_OS_Posix = false;
 
-enum TermDisp : uint {
-    RESET       = 0,
-    BRIGHT      = 1,
-    DIM         = 2,
-    UNDERLINE   = 4,
-    BLINK       = 5,
-    REVERSE     = 7,
-    HIDDEN      = 8,
+enum TermDisp : string {
+    RESET       = "0",
+    BRIGHT      = "1",
+    DIM         = "2",
+    UNDERLINE   = "4",
+    BLINK       = "5",
+    REVERSE     = "7",
+    HIDDEN      = "8",
     
-    FG_BLACK    = 30,
-    FG_RED      = 31,
-    FG_GREEN    = 32,
-    FG_YELLOW   = 33,
-    FG_BLUE     = 34,
-    FG_MAGENTA  = 35,
-    FG_CYAN     = 36,
-    FG_WHITE    = 37,
+    FG_BLACK    = "30",
+    FG_RED      = "31",
+    FG_GREEN    = "32",
+    FG_YELLOW   = "33",
+    FG_BLUE     = "34",
+    FG_MAGENTA  = "35",
+    FG_CYAN     = "36",
+    FG_WHITE    = "37",
     
-    BG_BLACK    = 40,
-    BG_RED      = 41,
-    BG_GREEN    = 42,
-    BG_YELLOW   = 43,
-    BG_BLUE     = 44,
-    BG_MAGENTA  = 45,
-    BG_CYAN     = 46,
-    BG_WHITE    = 47
+    BG_BLACK    = "40",
+    BG_RED      = "41",
+    BG_GREEN    = "42",
+    BG_YELLOW   = "43",
+    BG_BLUE     = "44",
+    BG_MAGENTA  = "45",
+    BG_CYAN     = "46",
+    BG_WHITE    = "47"
 }
 
 pure string esc(TermDisp[] attribs ...) {
