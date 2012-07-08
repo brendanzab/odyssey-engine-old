00d8968786d0ee07ab54111c9514517d59270439
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2012-07-08 22:25:04 +1000

    added col and row properties to Mat4

--- src/math/mat4.d
+++ src/math/mat4.d
@@ -22,8 +22,10 @@ struct Mat4 {
         };
         float[4][4] m;
     };
-
-        
+    
+    /**
+     *  Construct the matrix
+     */
     this(float m00, float m01, float m02, float m03,
          float m10, float m11, float m12, float m13,
          float m20, float m21, float m22, float m23,
@@ -35,11 +37,58 @@ struct Mat4 {
         m[3][0]=m30; m[3][1]=m31; m[3][2]=m32; m[3][3]=m33;
     }
     
-    this(float f) {
-        m[0][0]=f; m[0][1]=0; m[0][2]=0; m[0][3]=0;
-        m[1][0]=0; m[1][1]=f; m[1][2]=0; m[1][3]=0;
-        m[2][0]=0; m[2][1]=0; m[2][2]=f; m[2][3]=0;
-        m[3][0]=0; m[3][1]=0; m[3][2]=0; m[3][3]=f;
+    /**
+     *  Construct the matrix using column vectors
+     */
+    this(Vec4 col0, Vec4 col1, Vec4 col2, Vec4 col3) {
+        this.col!0 = col0;
+        this.col!1 = col1;
+        this.col!2 = col2;
+        this.col!3 = col3;
+    }
+    
+    /**
+     *  Set row
+     *      eg. m.row!3 = v
+     */
+    @property void row(int i)(Vec4 v) {
+        m[i][0] = v.v[0];
+        m[i][1] = v.v[1];
+        m[i][2] = v.v[2];
+        m[i][3] = v.v[3];
+    }
+    
+    /**
+     *  Get row as a Vec3
+     *      eg. m.row!1
+     */
+    @property Vec4 row(int i)() {
+        return Vec4(m[i][0],
+                    m[i][1],
+                    m[i][2],
+                    m[i][3]);
+    }
+    
+    /**
+     *  Set column
+     *      eg. m.col!3 = v
+     */
+    @property void col(int i)(Vec4 v) {
+        m[0][i] = v.v[0];
+        m[1][i] = v.v[1];
+        m[2][i] = v.v[2];
+        m[3][i] = v.v[3];
+    }
+    
+    /**
+     *  Get cloumn as a Vec3
+     *      eg. m.col!1
+     */
+    @property Vec4 col(int i)()  {
+        return Vec4(m[0][i],
+                    m[1][i],
+                    m[2][i],
+                    m[3][i]);
     }
     
     string toString() {
